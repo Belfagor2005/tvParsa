25e52f418f978466ffe1af035ebf54d1b9102921
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-08-28 20:00:05 +0200

    init fix

--- usr/lib/enigma2/python/Plugins/Extensions/parsatv/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/parsatv/Update.py
@@ -2,20 +2,33 @@ import os, re, sys
 from twisted.web.client import downloadPage
 PY3 = sys.version_info.major >= 3
 print("Update.py")
+
 def upd_done():        
+    from twisted.web.client import downloadPage
     print( "In upd_done")
     xfile ='http://patbuweb.com/parsatv/parsatv.tar'
     print('xfile: ', xfile)
     if PY3:
         xfile = b"http://patbuweb.com/parsatv/parsatv.tar"
-    print("Update.py not in PY3")
-    fdest = "/tmp/parsatv.tar"
-    print("upd_done xfile =", xfile)
-    downloadPage(xfile, fdest).addCallback(upd_last)
+        print("Update.py in PY3")
+    import requests
+    response = requests.head(xfile)
+    if response.status_code == 200:
+        # print(response.headers['content-length'])
+        fdest = "/tmp/parsatv.tar"
+        print("Code 200 upd_done xfile =", xfile)
+        downloadPage(xfile, fdest).addCallback(upd_last)
+    elif response.status_code == 404:
+        print("Error 404")
+    else:
+        return
 
-def upd_last(fplug): 
-    cmd = "tar -xvf /tmp/parsatv.tar -C /"
-    print( "cmd A =", cmd)
-    os.system(cmd)
-    pass
-
+def upd_last(fplug):
+    import time
+    time.sleep(5)        
+    if os.path.isfile('/tmp/parsatv.tar') and os.stat('/tmp/parsatv.tar').st_size > 10000 : 
+        import os
+        cmd = "tar -xvf /tmp/parsatv.tar -C /"
+        print( "cmd A =", cmd)
+        os.system(cmd)
+    return
--- usr/lib/enigma2/python/Plugins/Extensions/parsatv/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/parsatv/plugin.py
@@ -4,7 +4,7 @@
 ****************************************
 *        coded by Lululla & PCD        *
 *             skin by MMark            *
-*             15/07/2022               *
+*             25/08/2022               *
 *       Skin by MMark                  *
 ****************************************
 '''
@@ -1236,18 +1236,9 @@ def convert_bouquet(namex):
     Utils.web_info(message)
     Utils.ReloadBouquets()
 
-def intCheck():
-    import socket
-    try:
-        socket.setdefaulttimeout(1)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
-        return True
-    except:
-        return False
-
 def main(session, **kwargs):
     try:
-        if intCheck():
+        if Utils.zCheckInternet(0):
                 from . import Update
                 Update.upd_done()
                 session.open(MainParsa)
@@ -1260,17 +1251,7 @@ def main(session, **kwargs):
         traceback.print_exc() 
         pass
         
-# def main(session, **kwargs):
-    # from . import Utils
-    # if Utils.checkInternet():
-        # try:
-            # from . import Update
-            # Update.upd_done()
-        # except:
-            # pass
-        # session.open(MainParsa)
-    # else:
-        # session.open(MessageBox, "No Internet", MessageBox.TYPE_INFO)
+
 
 def StartSetup(menuid, **kwargs):
     if menuid == 'mainmenu':
